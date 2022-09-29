b400460b014c6b922bed77c4a3870581fab3a1ad
guilhermeOsilva <guilherme.osilva@bandtec.com.br>
2022-09-29 06:28:50 -0300

    iciando config do banco de dados

---
+++ db/conn.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+async function main() {
+    await mongoose.connect("mongodb://localhost:27017/getapet");
+    console.log("conexão bem sucedida");
+}
+
+main.catch(error => console.log(error))
+
+module.exports = mongoose;
