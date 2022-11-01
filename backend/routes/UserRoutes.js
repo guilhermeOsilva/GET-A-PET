f8cd9ae906900bffc8a67a5c9720c96917ae2d7f
guilhermeOsilva <guilherme.osilva@bandtec.com.br>
2022-10-31 23:00:08 -0300

    adicionando o helper na rota de eidção de usuario

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,11 +2,12 @@ const router = require("express").Router();
 const UserController = require("../controllers/UserController");
 
 const verifyToken = require("../helpers/verify-token");
+const { imageUpload } = require("../helpers/image-upload");
 
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.get("/checkuser", UserController.checkUser);
 router.get("/:id", UserController.getUserById);
-router.patch("/edit/:id", verifyToken, UserController.editUser);
+router.patch("/edit/:id", verifyToken, imageUpload.single("image"), UserController.editUser);
 
 module.exports = router;
